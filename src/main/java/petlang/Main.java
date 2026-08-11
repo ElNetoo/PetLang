5f234c5b5521f2c8176ba538111b5965e09dda28
ElNetoo <franciscokn7@gmail.com>
2026-08-11 16:38:14 -0300

    chore: Correção dos arquivos dos testes inválidos e válidos,

--- src/main/java/petlang/Main.java
+++ src/main/java/petlang/Main.java
@@ -22,36 +22,54 @@ public class Main {
         String source = Files.readString(Path.of(args[0]));
         CharStream input = CharStreams.fromString(source);
 
-
         PetLangLexer lexer = new PetLangLexer(input);
         lexer.removeErrorListeners();
+
+        final boolean[] lexError = {false};
         lexer.addErrorListener(new BaseErrorListener() {
             @Override
             public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,
                                     int line, int charPositionInLine, String msg,
                                     RecognitionException e) {
                 System.err.println("[Erro Léxico] Linha " + line + ":" + charPositionInLine + " - " + msg);
-                System.exit(1);
+                lexError[0] = true;
             }
         });
 
         CommonTokenStream tokens = new CommonTokenStream(lexer);
+        tokens.fill(); 
+
+
+        for (Token token : tokens.getTokens()) {
+            if (token.getType() == PetLangLexer.UNRECOGNIZED) {
+                System.err.println("[Erro Léxico] Linha " + token.getLine() +
+                        ":" + token.getCharPositionInLine() +
+                        " - caractere inválido: '" + token.getText() + "'");
+                lexError[0] = true;
+            }
+        }
+
+        if (lexError[0]) System.exit(1);
+        System.out.println("Análise léxica: OK");
 
         PetLangParser parser = new PetLangParser(tokens);
         parser.removeErrorListeners();
+
+        final boolean[] syntaxError = {false};
         parser.addErrorListener(new BaseErrorListener() {
             @Override
             public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,
                                     int line, int charPositionInLine, String msg,
                                     RecognitionException e) {
                 System.err.println("[Erro Sintático] Linha " + line + ":" + charPositionInLine + " - " + msg);
-                System.exit(1);
+                syntaxError[0] = true;
             }
         });
 
         ParseTree tree = parser.program();
-        System.out.println("Análise léxica e sintática: OK");
 
+        if (syntaxError[0]) System.exit(1);
+        System.out.println("Análise sintática: OK");
 
         try {
             new SemanticAnalyzer().visit(tree);
@@ -61,11 +79,9 @@ public class Main {
             System.exit(1);
         }
 
-
         Interpreter interpreter = new Interpreter();
         interpreter.visit(tree);
         System.out.println("Interpretação: OK");
-
         HtmlReportGenerator.generate(interpreter.getPets(), "relatorio.html");
     }
 }
